86b1cf514996c6851064b4c663e380738e0f2368
alabenkhlifa <ala.khliifa@gmail.com>
2025-04-15 19:32:12 +0100

    Implemented Profile Management

--- src/main/java/tn/ensit/soa/services/ProfileService.java
+++ src/main/java/tn/ensit/soa/services/ProfileService.java
@@ -2,42 +2,34 @@ package tn.ensit.soa.services;
 
 import org.springframework.stereotype.Service;
 import tn.ensit.soa.entities.Profile;
-import tn.ensit.soa.entities.User;
-import tn.ensit.soa.repositories.UserRepository;
+import tn.ensit.soa.repositories.ProfileRepository;
 
 import java.util.List;
 import java.util.Optional;
 
 // This annotation is required to declare a Service
 @Service
-public class UserService {
+public class ProfileService {
 
     // We are injecting the repository using the constructor (constructor based injection)
     // We don't need to initialize (or create) the repo ourselves, the Spring Framework will do it for us
     // Optionally we could have added an annotation @Autowired to specifically ask Spring to inject this field
     // But we didn't need it because Spring Framework is smart
-    private final UserRepository repository;
+    private final ProfileRepository repository;
 
-    private final ProfileService profileService;
-
-    public UserService(UserRepository repository, ProfileService profileService) {
+    public ProfileService(ProfileRepository repository) {
         this.repository = repository;
-        this.profileService = profileService;
     }
 
-    public List<User> getAllUsers() {
+    public List<Profile> getAllProfiles() {
         return repository.findAll();
     }
 
-    public Optional<User> getOneUser(Long id) {
+    public Optional<Profile> getOneProfile(Long id) {
         return repository.findById(id);
     }
 
-    public User createUser(User user) {
-        var createdUser = repository.save(user);
-        var bio = "";
-        Profile profile = new Profile(createdUser, bio);
-        profileService.createProfile(profile);
-        return createdUser;
+    public Profile createProfile(Profile profile) {
+        return repository.save(profile);
     }
 }
